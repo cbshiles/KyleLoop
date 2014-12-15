ce480c8e3f577c34a1494ba99215eeec345121d5
Carl Brenan Shiles <cbshiles@yahoo.com>
2014-12-15 13:30:48 -0500

    Routa

--- store.js
+++ store.js
@@ -4,17 +4,22 @@ var http = require('http'),
     url = require('url'),
     fs = require('fs');
 
-function doit(path, port){
-    http.createServer(function(req, res){ //request & response
+function direct(path){
+    if (path == '/')
+	path += 'index.html'
+    return '.'+path
+}
 
+function doit(port){
+    http.createServer(function(req, res){ //request & response
+	var path = direct(url.parse(req.url).pathname)
 	fs.readFile(path, function(err, data) {
 	    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})	    
 	    res.write(data)
-	    //res.write("<html><body><p>fdf dfdddd ddfd</p></body></html>")
 	    res.end()})
     }).listen(port, 'localhost')
     console.log('Server running.')
 }
 
 
-doit('./cherry.html', 8888)
+doit(8888)
